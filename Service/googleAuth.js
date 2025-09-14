d645ec9b70a78d1cd8f0a9a217c42db64bfd5af2
ann <annmariacgv@gmail.com>
2025-09-14 20:04:16 +0530

    few changes

--- Service/googleAuth.js
+++ Service/googleAuth.js
@@ -1,24 +1,22 @@
-const passport = require('passport')
-require('dotenv').config()
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const passport = require('passport');
+require('dotenv').config();
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CLIENT_CALLBACK
   },
-  
   function(accessToken, refreshToken, profile, done) {
-    
     return done(null, profile);
   }
 ));
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
